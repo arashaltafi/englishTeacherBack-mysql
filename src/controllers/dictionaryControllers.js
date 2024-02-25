3378b18d66d57f563f4f30d0520140fd821deba9
Arash Altafi <arashaltafi1377@gmail.com>
2024-02-25 11:39:36 +0330

    fix search api

--- src/controllers/dictionaryControllers.js
+++ src/controllers/dictionaryControllers.js
@@ -4,17 +4,17 @@ const searchDictionary = async (req, res, next) => {
     try {
         const { search, page_number, page_size } = req.query;
 
-        const pageOffset = parseInt(page_number - 1) * parseInt(page_size) || 1;
+        const pageOffset = (parseInt(page_number) - 1) * parseInt(page_size) || 0;
         const pageLimit = parseInt(page_size) || 1000;
 
-        if (pageOffset < 1 || pageLimit <= 0) {
+        if (pageOffset < 0 || pageLimit <= 0) {
             return res.status(400).send({
                 state: 'err',
                 message: 'لطفا مقادیر page_number و page_size را به صورت صحیح وارد نمایید',
             });
         }
 
-        const [searchItems] = await mysql.query(`SELECT * FROM dictionary WHERE word LIKE '%${search}%' LIMIT ${pageLimit} OFFSET ${pageOffset}`);
+        const [searchItems] = await mysql.query(`SELECT * FROM dictionary WHERE word LIKE '%${search}%' ORDER BY id ASC LIMIT ${pageOffset}, ${pageLimit}`);
 
         if (searchItems.length > 0) {
             const resultSearchData = {
