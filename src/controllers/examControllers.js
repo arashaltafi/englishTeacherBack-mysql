4911e35c8282c85cd04081d170143511878de1e0
Arash Altafi <arashaltafi1377@gmail.com>
2024-02-25 11:51:41 +0330

    fix paging exam

--- src/controllers/examControllers.js
+++ src/controllers/examControllers.js
@@ -6,8 +6,15 @@ const getAllHistory = async (req, res, next) => {
         const { grouping } = req.body;
         const { page_number, page_size } = req.query;
 
-        const pageOffset = parseInt(page_number - 1) * parseInt(page_size) || 1;
+        const pageOffset = (parseInt(page_number) - 1) * parseInt(page_size) || 0;
         const pageLimit = parseInt(page_size) || 1000;
+
+        if (pageOffset < 0 || pageLimit <= 0) {
+            return res.status(400).send({
+                state: 'err',
+                message: 'لطفا مقادیر page_number و page_size را به صورت صحیح وارد نمایید',
+            });
+        }
 
         if (!grouping) {
             return res.status(400).send({
